e64d340aad049edd8d34edb14064cde7b9685c90
chenxiaojing <chxj1992@gmail.com>
2016-01-09 15:32:45 +0800

    update kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('travelocity:crawl')->weekly()->saturdays()->at('13:00');
-
+//        $schedule->command('travelocity:crawl')->weekly()->saturdays()->at('13:00')->appendOutputTo('/tmp/travelocity.log');
+        $schedule->command('travelocity:crawl')->everyFiveMinutes()->appendOutputTo('/tmp/travelocity.log');
     }
 }
