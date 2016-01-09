0480f041d3cb3493739345b84e83ebd569b6765e
chenxiaojing <chxj1992@gmail.com>
2016-01-09 12:37:38 +0800

    update view

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -52,7 +52,7 @@
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav side-nav">
                 <li class="active">
-                    <a href="#"><i class="fa fa-fw fa-dashboard"></i> Travelocity </a>
+                    <a href="#"><i class="fa fa-fw fa-anchor"></i> Travelocity </a>
                 </li>
             </ul>
         </div>
